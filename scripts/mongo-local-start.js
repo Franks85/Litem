b6d2d16a741686e56b7d6a16f11ce379ae920b3f
Paolo Piaggesi <paolo.piaggesi@gmail.com>
2018-05-12 22:39:01 +0100

    create test user when local mongo starts

--- scripts/mongo-local-start.js
+++ scripts/mongo-local-start.js
@@ -1,4 +1,7 @@
 const MongoInMemory = require('mongo-in-memory')
+const User = require('../models/User')
+const mongoose = require('mongoose')
+const keys = require("../config/keys");
 
 const port = 27017
 const mongoServerInstance = new MongoInMemory(port)
@@ -9,5 +12,23 @@ mongoServerInstance.start((error, config) => {
   } else {
     var mongouri = mongoServerInstance.getMongouri('lost_test')
     console.log('mongo in memory started:', mongouri)
+
+    return createTestUser()
   }
 })
+
+async function createTestUser() {
+  mongoose.Promise = global.Promise;
+
+  await mongoose.connect(keys.mongoURI, {
+    useMongoClient: true
+  })
+
+  const user = new User()
+  user.email = 'test@litem.com'
+  user.password = user.generateHash('testuser')
+  await user.save(function (err) {
+    if (err) throw err
+    console.log('test user created successfully')
+  })
+}
